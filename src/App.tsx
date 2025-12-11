4020ca39a854a817a8a8d05d881dfc3b71411c24
jhonatta <jhonattasantoss@outlook.com>
2025-12-11 20:03:34 -0300

    feat: implement form components and integrate with company registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './App.css'
-import { Button } from './components/ui/button';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 function App() {
   const navigate = useNavigate();
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <h1>Bem-vindo ao SmartPurchase</h1>
+      <Input placeholder="Pesquisar..." />
       <nav>
         <Button onClick={() => navigate("/company/register")}>Cadastrar Empresa</Button>
       </nav>
--- src/pages/Company/Register/index.tsx
+++ src/pages/Company/Register/index.tsx
@@ -4,6 +4,16 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useRegisterCompany } from '@/hooks/useRegisterCompany';
 import type { CompanySchema } from '@/schemas/companySchema';
 import { companySchema } from '@/schemas/companySchema';
+import { Button } from '@/components/ui/button';
+import { Input } from "@/components/ui/input"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSet,
+} from "@/components/ui/field"
 import './styles.css';
 
 const CompanyRegistration: React.FC = () => {
@@ -23,29 +33,45 @@ const CompanyRegistration: React.FC = () => {
   return (
     <div className="registration-container">
       <form className="registration-form" onSubmit={handleSubmit(onSubmit)}>
-        <h1>Cadastro de Empresa</h1>
-        
         {isSuccess && <p className="success-message">Empresa cadastrada com sucesso!</p>}
         {error && <p className="error-message">{error}</p>}
 
-        <div className="form-group">
-          <label htmlFor="name">Nome da Empresa</label>
-          <input type="text" id="name" {...register('name')} />
-          {errors.name && <p className="error-message">{errors.name.message}</p>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="cnpj">CNPJ</label>
-          <input type="text" id="cnpj" {...register('cnpj')} />
-          {errors.cnpj && <p className="error-message">{errors.cnpj.message}</p>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">E-mail</label>
-          <input type="email" id="email" {...register('email')} />
-          {errors.email && <p className="error-message">{errors.email.message}</p>}
-        </div>
-        <button type="submit" className="submit-button" disabled={isLoading}>
-          {isLoading ? 'Cadastrando...' : 'Cadastrar'}
-        </button>
+        <FieldGroup>
+          <FieldSet>
+            <FieldLegend>Formulário de Cadastro</FieldLegend>
+            <FieldDescription>Preencha os dados da empresa abaixo:</FieldDescription>
+
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="name">Nome da Empresa</FieldLabel>
+                <Input type="text" id="name" {...register('name')} placeholder='Digite o nome da empresa' />
+                {errors.name && <p className="error-message">{errors.name.message}</p>}
+              </Field>
+            </FieldGroup>
+
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="cnpj">CNPJ</FieldLabel>
+                <Input type="text" id="cnpj" {...register('cnpj')} placeholder='Digite o CNPJ da empresa' />
+                {errors.cnpj && <p className="error-message">{errors.cnpj.message}</p>}
+              </Field>
+            </FieldGroup>
+
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">E-mail</FieldLabel>
+                <Input type="email" id="email" {...register('email')} placeholder='Digite o e-mail da empresa' />
+                {errors.email && <p className="error-message">{errors.email.message}</p>}
+              </Field>
+            </FieldGroup>
+
+            <Button disabled={isLoading} variant="outline" type="submit">
+              {isLoading ? 'Cadastrando...' : 'Cadastrar'}
+            </Button>
+
+          </FieldSet>
+        </FieldGroup>
+        
       </form>
     </div>
   );
